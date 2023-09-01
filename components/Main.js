42cb1937d84c4a15941eabf7811d554d3d1ae7ed
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2023-09-01 12:44:51 +0300

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -126,7 +126,7 @@ export default function Main() {
                     <h1 className={style.titleprojects}>Romdale Store</h1>
                     <p>
                         A detailed e-commerce store I built from scratch using Next.js and React Contexts. 
-                        You can navigate only from homepage to collections: hats, shoes, ID dynamic route for each hats and shoes, authentication page, other route pages, checkout page, wishlist page only for functionalities and viewing.
+                        You can navigate only from homepage to collections: hats, shoes, ID dynamic route for each hats and shoes, authentication page, other route pages, checkout page, wishlist page only for functionalities and viewing (payment unauthorized).
                         Available on both desktop browsers and mobile versions. Site deployed with Vercel. Added the github repo link from this project as well.
                         Tools used:
                     </p>
